bd99353db396237c06c01fd7e6ec324e40a3d1bf
Tedozi Manson <duyphan.developer@gmail.com>
2016-12-07 00:43:55 +0700

    Refactor Make routes to Laravel 5.3 style

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php use Illuminate\Routing\Router;
+
+/**
+ *
+ * @var Router $router
+ *
+ */
+
+$router->group(['middleware' => 'web'], function (Router $router) {
+
+    $adminRoute = config('webed.admin_route');
+
+    $moduleRoute = 'auth';
+
+    /*
+     * Admin route
+     * */
+    $router->group(['prefix' => $adminRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
+        $router->get($moduleRoute, function () use ($adminRoute, $moduleRoute) {
+            return redirect()->to($adminRoute . '/' . $moduleRoute . '/login');
+        });
+
+        $router->group(['prefix' => $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
+            $router->get('login', 'AuthController@getLogin')->name('admin::auth.login.get');
+            $router->post('login', 'AuthController@postLogin')->name('admin::auth.login.post');
+            $router->get('logout', 'AuthController@getLogout')->name('admin::auth.logout.get');
+        });
+    });
+});
--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -9,27 +9,7 @@ class RouteServiceProvider extends ServiceProvider
 
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace, 'middleware' => 'web'], function (Router $router) {
-
-            $adminRoute = config('webed.admin_route');
-
-            $moduleRoute = 'auth';
-
-            /*
-             * Admin route
-             * */
-            $router->group(['prefix' => $adminRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
-                $router->get($moduleRoute, function () use ($adminRoute, $moduleRoute) {
-                    return redirect()->to($adminRoute . '/' . $moduleRoute . '/login');
-                });
-
-                $router->group(['prefix' => $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
-                    $router->get('login', 'AuthController@getLogin')->name('admin::auth.login.get');
-                    $router->post('login', 'AuthController@postLogin')->name('admin::auth.login.post');
-                    $router->get('logout', 'AuthController@getLogout')->name('admin::auth.logout.get');
-                });
-            });
-
-        });
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
     }
 }
